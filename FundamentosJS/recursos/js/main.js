43b34b7c157ee79a767a0ec891ae4edc3f3957da
Andres Correa <andrescorrea0599@gmail.com>
2024-11-07 17:39:52 -0500

    Se crea el proyecto con los ejemplos de los fundamentos de javaScript

---
+++ FundamentosJS/recursos/js/main.js
@@ -0,0 +1,44 @@
+/*METODO MAS USADO*/
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("demo").style.fontSize = "35px"
+
+
+/*sALIDA DE DATOS*/
+
+window.alert(5+2)
+console.log (56+4)
+
+
+/* Declaracion */
+
+let a,b,c;
+
+a= 5;
+b= 6;
+c = a + b;
+
+window.alert(c)
+document.getElementById("demo").innerHTML = c;
+
+/* VARIABLES */
+
+let p,q,r;
+
+const m = 8;
+
+r = p + q + m;
+
+document.getElementById("demo1").innerHTML = r;
+
+/*  TIPOS DE DATOS  */
+
+
+let num = 20; //numero entero
+let iscolombiano = true; //booleano
+let name = "Neymar pele vinicius";// cadena
+let km = 14.15; //numero decimal
+let obj = {fisrtname: "Ana", lastname: "Perez"}; // objeto
+
+let y = 17 + "POO";
+
+window.alert(y);
